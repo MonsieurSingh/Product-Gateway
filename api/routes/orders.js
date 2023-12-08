9b7d4a6ff196a71e4c60dd04646e611ee9d11584
MonsieurSingh <66196010+MonsieurSingh@users.noreply.github.com>
2023-12-08 23:02:57 +1100

    Refactor product routes to use async/await

--- api/routes/orders.js
+++ api/routes/orders.js
@@ -4,127 +4,119 @@ const mongoose = require('mongoose');
 const Order = require('../models/order');
 const Product = require('../models/product');
 
-router.get('/', (req, res, next) => {
-	Order.find()
-		.select('product quantity _id')
-		// .populate('product', 'name')
-		.exec()
-		.then((docs) => {
-			res.status(200).json({
-				count: docs.length,
-				orders: docs.map((doc) => {
-					return {
-						_id: doc._id,
-						product: doc.product,
-						quantity: doc.quantity,
-						request: {
-							type: 'GET',
-							url: 'http://localhost:3000/orders/' + doc._id,
-						},
-					};
-				}),
-			});
-		})
-		.catch((err) => {
-			res.status(500).json({
-				error: err,
-			});
-		});
+// GET route (all orders)
+router.get('/', async (req, res, next) => {
+    console.log("hello")
+    try {
+        const docs = await Order.find().select('product quantity _id').exec();
+        res.status(200).json({
+            count: docs.length,
+            orders: docs.map((doc) => {
+                return {
+                    _id: doc._id,
+                    product: doc.product,
+                    quantity: doc.quantity,
+                    request: {
+                        type: 'GET',
+                        url: 'http://localhost:3000/orders/' + doc._id,
+                    },
+                };
+            }),
+        });
+    } catch (err) {
+        res.status(500).json({
+            error: err,
+        });
+    }
 });
 
-router.post('/', (req, res, next) => {
-	const productId = req.body.productId;
-	Product.findById(productId)
-		.then((product) => {
-			if (!product) {
-				return res.status(404).json({
-					message: 'Product not found',
-				});
-			}
-			const order = new Order({
-				_id: new mongoose.Types.ObjectId(),
-				product: productId,
-				quantity: req.body.quantity,
-			});
-			return order.save();
-		})
-		.then((result) => {
-			console.log(result);
-			res.status(201).json({
-				message: 'Order stored',
-				createdOrder: {
-					_id: result._id,
-					product: result.product,
-					quantity: result.quantity,
-				},
-				request: {
-					type: 'GET',
-					url: 'http://localhost:3000/orders/' + result._id,
-				},
-			});
-		})
-		.catch((err) => {
-			console.log(err);
-			res.status(500).json({
-				error: err,
-			});
-		});
-});
-
-router.get('/:orderId', (req, res, next) => {
-	const orderId = req.params.orderId;
-	Order.findById(orderId)
-		.select('product quantity _id')
-		.exec()
-		.then((order) => {
-			if (!order) {
-				return res.status(404).json({
-					message: 'Order not found',
-				});
-			}
-			res.status(200).json({
-				order: {
-					_id: order._id,
-					product: order.product,
-					quantity: order.quantity,
-				},
-				request: {
-					type: 'GET',
-					url: 'http://localhost:3000/orders',
-				},
-			});
-		})
-		.catch((err) => {
-			res.status(500).json({
-				error: err,
-			});
-		});
-});
-
-router.delete('/:orderId', (req, res, next) => {
-    const orderId = req.params.orderId;
-	Order.findByIdAndDelete(orderId)
-		.exec()
-        .then((result) => {
-            if (!result) {
-                return res.status(404).json({
-                    message: 'Order not found',
-                });
-            }
-            res.status(200).json({
-                message: 'Order deleted',
-                request: {
-                    type: 'POST',
-                    url: 'http://localhost:3000/orders',
-                    body: { productId: req.body.productId, quantity: req.body.quantity },
-                },
+// POST route (create order)
+router.post('/', async (req, res, next) => {
+    try {
+        const productId = req.body.productId;
+        const product = await Product.findById(productId);
+        if (!product) {
+            return res.status(404).json({
+                message: 'Product not found',
             });
-        })
-		.catch((err) => {
-			res.status(500).json({
-				error: err,
-			});
-		});
+        }
+        const order = new Order({
+            _id: new mongoose.Types.ObjectId(),
+            product: productId,
+            quantity: req.body.quantity,
+        });
+        const result = await order.save();
+        console.log(result);
+        res.status(201).json({
+            message: 'Order stored',
+            createdOrder: {
+                _id: result._id,
+                product: result.product,
+                quantity: result.quantity,
+            },
+            request: {
+                type: 'GET',
+                url: 'http://localhost:3000/orders/' + result._id,
+            },
+        });
+    } catch (err) {
+        console.log(err);
+        res.status(500).json({
+            error: err,
+        });
+    }
+});
+// GET/:orderId route (get order by id)
+router.get('/:orderId', async (req, res, next) => {
+    try {
+        const order = await Order.findById(req.params.orderId).select('product quantity _id').exec();
+        if (!order) {
+            return res.status(404).json({
+                message: 'Order not found'
+            });
+        }
+        res.status(200).json({
+            order: order,
+            request: {
+                type: 'GET',
+                url: 'http://localhost:3000/orders'
+            }
+        });
+    } catch (err) {
+        res.status(500).json({
+            error: err
+        });
+    }
+});
+
+// DELETE/:orderId route (delete order by id)
+router.delete('/:orderId', async (req, res, next) => {
+    console.log("hello")
+    try {
+        const orderId = req.params.orderId;
+        console.log('req.params', req.params)
+        console.log('orderId', orderId)
+        console.log('req.params', req.params)
+        const result = await Order.findByIdAndDelete(orderId).exec();
+        console.log('result', result)
+        if (!result) {
+            return res.status(404).json({
+                message: 'Order not found',
+            });
+        }
+        res.status(200).json({
+            message: 'Order deleted',
+            request: {
+                type: 'POST',
+                url: 'http://localhost:3000/orders',
+                body: { orderId: req.params.orderId, productId: result.product, quantity: result.quantity },
+            },
+        });
+    } catch (err) {
+        res.status(500).json({
+            error: err,
+        });
+    }
 });
 
 module.exports = router;
--- api/routes/products.js
+++ api/routes/products.js
@@ -3,134 +3,134 @@ const router = express.Router();
 const mongoose = require('mongoose');
 const Product = require('../models/product');
 
-router.get('/', (req, res, next) => {
-	Product.find()
-		.select('name price _id')
-		.exec()
-		.then((docs) => {
-			const response = {
-				count: docs.length,
-				products: docs.map((doc) => {
-					return {
-						name: doc.name,
-						price: doc.price,
-						_id: doc._id,
-						request: {
-							type: 'GET',
-							url: 'http://localhost:3000/products/' + doc._id,
-						},
-					};
-				}),
-			};
-			res.status(200).json({
-				message: 'Handling GET requests to /products',
-				products: response,
-			});
-		})
-		.catch((err) => {
-			console.log(err);
-			res.status(500).json({ error: err });
-		});
-});
-
-router.post('/', (req, res, next) => {
-	const product = new Product({
-		_id: new mongoose.Types.ObjectId(),
-		name: req.body.name,
-		price: req.body.price,
-	});
-	product
-		.save()
-		.then((result) => {
-			res.status(201).json({
-				message: 'Created product successfully',
-				createdProduct: {
-					name: result.name,
-					price: result.price,
-					_id: result._id,
+// GET route (all products)
+router.get('/', async (req, res, next) => {
+	try {
+		const docs = await Product.find().select('name price _id').exec();
+		const response = {
+			count: docs.length,
+			products: docs.map((doc) => {
+				return {
+					name: doc.name,
+					price: doc.price,
+					_id: doc._id,
 					request: {
 						type: 'GET',
-						url: 'http://localhost:3000/products/' + result._id,
+						url: 'http://localhost:3000/products/' + doc._id,
 					},
-				},
-			});
-		})
-		.catch((err) => {
-			console.log(err);
-			res.status(500).json({ error: err });
+				};
+			}),
+		};
+		res.status(200).json({
+			message: 'Handling GET requests to /products',
+			products: response,
 		});
-});
-
-router.get('/:productId', (req, res, next) => {
-	const id = req.params.productId;
-	Product.findById(id)
-		.select('name price _id')
-		.exec()
-		.then((doc) => {
-			console.log('From database', doc);
-			if (doc) {
-				res.status(200).json({
-					product: doc,
-					request: {
-						type: 'GET',
-						description: 'Get all products',
-						url: 'http://localhost:3000/products',
-					},
-				});
-			} else {
-				res.status(404).json({
-					message: 'No valid entry found for provided ID',
-				});
-			}
-		})
-		.catch((err) => {
-			console.log(err);
-			res.status(500).json({ error: err });
-		});
-});
-
-router.patch('/:productId', (req, res, next) => {
-	const id = req.params.productId;
-	const updateOps = {};
-	for (const ops of req.body) {
-		// req.body is an array of objects
-		updateOps[ops.propName] = ops.value;
+	} catch (err) {
+		console.log(err);
+		res.status(500).json({ error: err });
 	}
-	Product.updateOne({ _id: id }, { $set: updateOps })
-		.exec()
-		.then((result) => {
-			res.status(200).json({
-				message: 'Product updated!',
+});
+
+// POST route (create product)
+router.post('/', async (req, res, next) => {
+	try {
+		const product = new Product({
+			_id: new mongoose.Types.ObjectId(),
+			name: req.body.name,
+			price: req.body.price,
+		});
+		const result = await product.save();
+		res.status(201).json({
+			message: 'Created product successfully',
+			createdProduct: {
+				name: result.name,
+				price: result.price,
+				_id: result._id,
 				request: {
 					type: 'GET',
-					url: 'http://localhost:3000/products/' + id,
+					url: 'http://localhost:3000/products/' + result._id,
 				},
-			});
-		})
-		.catch((err) => {
-			console.log(err);
-			res.status(500).json({ error: err });
+			},
 		});
+	} catch (err) {
+		console.log(err);
+		res.status(500).json({ error: err });
+	}
 });
 
-router.delete('/:productId', (req, res, next) => {
-	const id = req.params.productId;
-	Product.deleteOne({ _id: id })
-		.exec()
-		.then((result) => {
+// GET/:productId route (get product by ID)
+router.get('/:productId', async (req, res, next) => {
+	try {
+		const id = req.params.productId;
+		const doc = await Product.findById(id).select('name price _id').exec();
+		console.log('From database', doc);
+		if (doc) {
 			res.status(200).json({
-				message: 'Product deleted!',
+				product: doc,
 				request: {
-					type: 'POST',
+					type: 'GET',
+					description: 'Get all products',
 					url: 'http://localhost:3000/products',
-					body: { name: 'String', price: 'Number' },
 				},
 			});
-		})
-		.catch((err) => {
-			console.log(err);
-			res.status(500).json({ error: err });
+		} else {
+			res.status(404).json({
+				message: 'No valid entry found for provided ID',
+			});
+		}
+	} catch (err) {
+		console.log(err);
+		res.status(500).json({ error: err });
+	}
+});
+
+// PATCH/:productId route (update product by ID)
+router.patch('/:productId', async (req, res, next) => {
+	try {
+		const id = req.params.productId;
+		const updateOps = {};
+		for (const ops of req.body) {
+			// req.body is an array of objects
+			updateOps[ops.propName] = ops.value;
+		}
+		await Product.updateOne({ _id: id }, { $set: updateOps }).exec();
+		res.status(200).json({
+			message: 'Product updated!',
+			request: {
+				type: 'GET',
+				url: 'http://localhost:3000/products/' + id,
+			},
 		});
+	} catch (err) {
+		console.log(err);
+		res.status(500).json({ error: err });
+	}
+});
+
+// DELETE/:productId route (delete product by ID)
+router.delete('/:productId', async (req, res, next) => {
+	try {
+		const id = req.params.productId;
+		console.log('id', id)
+		console.log('req.params', req.params)
+		console.log('req.body', req.body)
+		console.log('req.params.productId', req.params.productId)
+		await Product.deleteOne({ _id: id }).exec();
+		res.status(200).json({
+			message: 'Product deleted!',
+			request: {
+				type: 'POST',
+				url: 'http://localhost:3000/products',
+				body: { 
+					name: req.body.name, 
+					price: req.body.price, 
+					productId: req.params.productId },
+			},
+		});
+	} catch (err) {
+		console.log(err);
+		res.status(500).json({ error: err });
+	}
 });
 
 module.exports = router;
